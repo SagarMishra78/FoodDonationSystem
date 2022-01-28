fff9df39e4dedbfbeea17d1a425109e33b487e6b
Sagar <smishra787898@gmail.com>
2022-01-28 15:27:52 +0530

    req

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -45,31 +45,31 @@ const Home = () => {
               bulk of the card's content.
             </p>
             <Link className="btn btn-primary" to={"/RequestDonation"}>
-              Go somewhere
+              Request
             </Link>
           </div>
         </div>
         <div className="card" style={{ width: "18rem" }}>
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title">Confirm Donation</h5>
             <p className="card-text">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </p>
             <Link className="btn btn-primary" to={"/"}>
-              Go somewhere
+              Confirm
             </Link>
           </div>
         </div>
         <div className="card" style={{ width: "18rem" }}>
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title">Track Donation</h5>
             <p className="card-text">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </p>
             <Link className="btn btn-primary" to={"/"}>
-              Go somewhere
+              Track
             </Link>
           </div>
         </div>
