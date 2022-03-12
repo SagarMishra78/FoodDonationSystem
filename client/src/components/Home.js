4d348b761f5b66ae03fb3bc9cbc6890322a0adc4
Sagar Mishra <smishra787898@gmail.com>
2022-03-12 12:44:04 +0530

    confirm donation

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -63,7 +63,7 @@ const Home = () => {
         </div>
         <div className="card" style={{ width: "18rem" }}>
           <div className="card-body">
-            <h5 className="card-title">Track Donation</h5>
+            <h5 className="card-title">Donation Status</h5>
             <p className="card-text">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
