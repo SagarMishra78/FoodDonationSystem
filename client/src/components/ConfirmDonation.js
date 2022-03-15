3e36a5eb28bf58390fd49fc089034cfa616609d3
Sagar Mishra <smishra787898@gmail.com>
2022-03-15 20:55:29 +0530

    confirm

--- client/src/components/ConfirmDonation.js
+++ client/src/components/ConfirmDonation.js
@@ -57,8 +57,25 @@ const ConfirmDonation = () => {
   };
 
   const handleConfirm = async (e) => {
-    // let id = e.currentTarget.id;
+    let id = e.currentTarget.id;
     Navigate("/confirmdonationmodal");
+    const res = await fetch("/delete", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id,
+      }),
+    });
+    await res.json();
+    if (res.status === 200) {
+      toast.success("Submit Confirmaion Details", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: true,
+        hideProgressBar: true,
+      });
+    }
   };
 
   const DisplayData = requests.map((info, i) => {
